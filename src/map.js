b4b8ad76e94434b403f59dc65f2fdc8d73d2e2fc
Atharva Patil <patil8atharva@gmail.com>
2024-07-08 10:15:03 +0530

    Brightness and Contrast Sliders

--- src/map.js
+++ src/map.js
@@ -1,11 +1,12 @@
 import { View, Map } from 'ol';
 import OSM from 'ol/source/OSM';
-import Tile from 'ol/layer/Tile';
+//import Tile from 'ol/layer/Tile';
+import TileLayer from 'ol/layer/WebGLTile';
 import { Progress } from './progress';
-import {NotificationControl, DirectionControl} from './map_controls';
+import { NotificationControl, DirectionControl } from './map_controls';
 import { initIconMode, handleIconMode } from './icon';
 import { initDirectionMode, handleDirectionMode } from './route';
-import {defaults as defaultControls} from 'ol/control';
+import { defaults as defaultControls } from 'ol/control';
 
 const progress = new Progress(document.getElementById('progress'));
 const source = new OSM();
@@ -18,17 +19,26 @@ source.on(['tileloadend', 'tileloaderror'], function () {
     progress.addLoaded();
 });
 
-function toggleMode(mode){
+function toggleMode(mode) {
     directionMode = mode;
 }
-
+const imageVariables = {
+    brightness: 0,
+    contrast: 0
+};
+const tileLayer = new TileLayer({
+    source: source,
+    style: {
+        contrast: ['var', 'contrast'],
+        brightness: ['var', 'brightness'],
+        variables: imageVariables
+    }
+});
 const map = new Map({
     target: 'map',
     controls: defaultControls().extend([new NotificationControl(), new DirectionControl(toggleMode, directionMode)]),
     layers: [
-        new Tile({
-            source: source,
-        }),
+        tileLayer
     ],
     view: new View({
         center: [0, 0],
@@ -53,3 +63,19 @@ map.on("click", async function (event) {
         await handleDirectionMode(event);
     }
 });
+
+
+for (let variable in imageVariables) {
+    const name = variable;
+    const element = document.getElementById(name);
+    const value = imageVariables[name];
+    element.value = value.toString();
+    document.getElementById(name + '-value').innerText = value.toFixed(2);
+    element.addEventListener('input', function (event) {
+        const value = parseFloat(event.target.value);
+        document.getElementById(name + '-value').innerText = value.toFixed(2);
+        const updates = {};
+        updates[name] = value;
+        tileLayer.updateStyleVariables(updates);
+    });
+} 
